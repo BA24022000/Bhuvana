a1793ff6ab9e89a49fb9e925ca34375146e854a0
BA24022000 <47380408+BA24022000@users.noreply.github.com>
2019-02-27 15:14:56 +0530

    Create alphabetornot.java

---
+++ alphabetornot.java
@@ -0,0 +1,22 @@
+# Bhuvana
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		char b=in.next().charAt(0);
+		if((b>='a'&&b<='z')||(b>='A'&&b<='z'))
+		{ 
+			System.out.println("Alphabet");
+		}
+		else
+		{
+		System.out.println("NO");	
+		}
+	}
+}
